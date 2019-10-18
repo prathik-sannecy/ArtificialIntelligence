7ae12a18beeabfaf6c844f01ff2e66b778952e3e
psannecy <prathik.sannecy@intel.com>
2019-10-17 18:18:57 -0700

    Adding main run file

---
+++ HW1/ParseInputFile.py
@@ -0,0 +1,11 @@
+from Optomize import *
+
+def Parse(file):
+    """Parse the file, returns back all the people at the table
+
+    inputs:
+        (string) file name in given format
+    returns:
+        (list of People) all the people in the dinner table initialized
+    """
+    pass
