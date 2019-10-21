410f4e2fd41c5e571e161197b67a861a77c1c56c
psannecy <prathik.sannecy@intel.com>
2019-10-21 12:31:34 -0700

    Adding comments

--- HW1/DinnerTableMain.py
+++ HW1/DinnerTableMain.py
@@ -3,10 +3,29 @@ from Optomize import *
 import sys
 
 
-def Main(input_file) :
-    sys.setrecursionlimit(10000)
+def Main(input_file):
+    """Finds the optimized seating arrangement for the dinner table for the highest possible score
+
+    The "score" of a table is determined by the following criteria:
+
+    1 point for every adjacent pair (seated next to each other) of people with one a host and the other a guest. 2
+    points for every opposite pair (seated across from each other) of people with one a host and the other a guest.
+    h(p1, p2) + h(p2, p1) points for every adjacent or opposite pair of people p1, p2 where h(p1,2) is the preference
+        function between two people
+
+    inputs:
+        File name for initializing the dinner table
+    returns:
+        None
+
+    """
+    # Increase the recursion limit max (used for the optomization function)
+    sys.setrecursionlimit(100000)
+    # Get all the people at the dinner table
     persons = Parse(input_file)
+    # Optimize the seating arrangements for the highest score
     score, people = Optomize(persons, 0, 1)
+    # Print the result in the desired format
     print(score)
     people.sort(key=lambda x: x.GetNumber())
     for person in people:
@@ -15,5 +34,6 @@ def Main(input_file) :
 
 if __name__ == '__main__':
     print("Enter Path to file")
+    # Get the input file (to initialize dinner table)
     file_path = input()
     Main(file_path)
--- HW1/ParseInputFile.py
+++ HW1/ParseInputFile.py
@@ -12,15 +12,18 @@ def Parse(input_file):
     try:
         with open(input_file) as file:
             num_people = int(file.readline())
+            # First half of people are hosts
             for i in range(1, int(num_people / 2) + 1):
                 person = Person(i, "H")
                 person.SetPosition(i)
                 people.append(person)
+            # Second half of people are guests
             for i in range(int(num_people / 2) + 1, num_people + 1):
                 person = Person(i, "G")
                 person.SetPosition(i)
                 people.append(person)
 
+            # Set the first half of the people on the top row (looking top-down)
             for i in range(0, int(num_people / 2) - 1):
                 people[i].SetAdjRight(people[i + 1])
             for i in range(1, int(num_people / 2)):
@@ -28,6 +31,7 @@ def Parse(input_file):
             for i in range(0, int(num_people / 2)):
                 people[i].SetDown(people[i + int(num_people / 2)])
 
+            # Set the second half of the people on the bottom row (looking top-down)
             for i in range(int(num_people / 2), num_people - 1):
                 people[i].SetAdjRight(people[i + 1])
             for i in range(1 + int(num_people / 2), num_people):
@@ -35,6 +39,7 @@ def Parse(input_file):
             for i in range(int(num_people / 2), num_people):
                 people[i].SetUp(people[i - int(num_people / 2)])
 
+            # Set the relation between each person
             person_num = 0
             for line in file:
                 relations = line.strip("\n").split()
