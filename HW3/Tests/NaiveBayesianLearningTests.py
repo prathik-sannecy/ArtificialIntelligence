adada0d48fc5fc6c20e72bb2a8dc1a7466366f13
prathik <prathik@pdx.edu>
2019-11-09 23:26:28 -0800

    Removed src code from src directory (can't run unit level tests)

---
+++ HW3/Tests/NaiveBayesianLearningTests.py
@@ -0,0 +1,11 @@
+import unittest
+from Learning import *
+
+
+class NaiveBayesianLearningTests(unittest.TestCase):
+    def test_Learn(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
