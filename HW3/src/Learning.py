b33c5787d44005ef3ffd9fa9ed7118428336e6e9
prathik <prathik@pdx.edu>
2019-11-09 23:21:27 -0800

    Added comments

--- HW3/src/Learning.py
+++ HW3/src/Learning.py
@@ -4,14 +4,14 @@ def Learn(trainingSet):
     """Naive Bayesian learning algorithm
 
     inputs:
-        (List) Training set in the following format:
+        (List)trainingSet: Training set in the following format:
         Instance0 classification, Instance0 feature0, Instance0 feature1...Instance0 featureN
         Instance1 classification, Instance0 feature1, Instance1 feature1...Instance1 featureN
         ...
 
     returns:
-        (tuple) (count of instances with a classification,
-        count of instances with a particular feature within a classification)
+        (tuple)(instanceCount, featureCount): (count of instances with a classification,
+            count of instances with a particular feature within a classification)
     """
     classificationIndex = 0
     featureStartIndex = 1  # Feature offset in an instance
@@ -34,8 +34,21 @@ def Learn(trainingSet):
     return instanceCount, featureCount
 
 def ComputeLikelyhoodOfClassification(instanceFeatures, featureCount, instanceCount, classification):
+    """Returns the likelyhood that a particular feature list is of a certain classification based on Naive Bayesian learning algorithm
+
+    inputs:
+        (list)instanceFeatures: a feature list to be classified
+        (list)featureCount: how many total(all instances) features are in each classification are in the training set
+        (list)instanceCount: how many instances are in each classification are in the training set
+        (int)classification: whether the feature list should be classified as '0' or '1'
+
+    returns:
+        (float)likelyHood: how likely the feature list fits the classification, based on the training set
+    """
+    # default is that likelyhood is dependant on how many instances we've already seen
     likelyHood = math.log(instanceCount[classification] + .5) - math.log(instanceCount[0] + instanceCount[1] + .5)
     numFeatures = len(instanceFeatures)
+    # Adjust the likelyhood based on how many features of the classification it matches
     for featureIndex in range(numFeatures):
         countOfFeature = featureCount[classification][featureIndex]
         if instanceFeatures[featureIndex] == 0:
@@ -44,6 +57,16 @@ def ComputeLikelyhoodOfClassification(instanceFeatures, featureCount, instanceCo
     return likelyHood
 
 def classifyInstance(instanceFeatures, featureCount, instanceCount):
+    """Returns how a feature list should be classified based on likelyhood
+
+    inputs:
+        (list)instanceFeatures: a feature list to be classified
+        (list)featureCount: how many total(all instances) features are in each classification are in the training set
+        (list)instanceCount: how many instances are in each classification are in the training set
+
+    returns:
+        (int)classification: whether the feature list should be classified as '0' or '1'
+    """
     if ComputeLikelyhoodOfClassification(instanceFeatures, featureCount, instanceCount, 1) > ComputeLikelyhoodOfClassification(instanceFeatures, featureCount, instanceCount, 0):
         return 1
     return 0
