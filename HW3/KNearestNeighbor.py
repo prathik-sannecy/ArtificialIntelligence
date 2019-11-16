747b0f0e6d69dc11f6ccdec55ddc97ab70162bfd
prathik <prathik@pdx.edu>
2019-11-16 12:07:18 -0800

    Adding comments to KNearestNeighbor algorithm

--- HW3/KNearestNeighbor.py
+++ HW3/KNearestNeighbor.py
@@ -1,4 +1,18 @@
 def ClassifyKNearestNeighbor(trainingSet, newInstanceFeatures, k):
+    """Returns how a feature list should be classified based on how well it matches to its nearest neighbors (k
+    nearest neighbors algorithm)
+
+    inputs:
+        (List[(int)])trainingSet: Training set in the following format:
+            Instance0 classification, Instance0 feature0, Instance0 feature1...Instance0 featureN
+            Instance1 classification, Instance0 feature1, Instance1 feature1...Instance1 featureN
+        (list[(int)])newInstanceFeatures: a feature list to be classified
+        (int)k: how many nearest neighbors to compare to
+
+    returns:
+        (int)classification: whether the feature list should be classified as '0' or '1'
+    """
+
     difference = []
     classificationIndex = 0
     featureStartIndex = 1  # Feature offset in an instance
