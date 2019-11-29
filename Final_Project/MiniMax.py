91ab9f5795c9b964e5b2aa9e0519bc537f81fbbf
prathik <prathik@pdx.edu>
2019-11-28 20:35:06 -0800

    Using functions to access State variables, rather than accessing them directly

--- Final_Project/MiniMax.py
+++ Final_Project/MiniMax.py
@@ -10,17 +10,17 @@ def MiniMaxDecision(state):
 
 def MinValue(state):
     if state.TerminalTest():
-        return state.utility
+        return state.Utility()
     minValue = float("inf")
-    for action in state.actions:
-        minValue = min(MaxValue(action), minValue)
+    for action in state.GetActions():
+        minValue = min(MaxValue(state.Result(action)), minValue)
     return minValue
 
 def MaxValue(state):
     if state.TerminalTest():
-        return state.utility
+        return state.Utility()
     maxValue = float("-inf")
-    for action in state.actions:
-        maxValue = min(MinValue(action), maxValue)
+    for action in state.GetActions():
+        maxValue = max(MinValue(state.Result(action)), maxValue)
     return maxValue
 
