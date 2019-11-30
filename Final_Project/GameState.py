8b95ea3657c8d6fc2537c5474d6c2d77ace8211d
prathik <prathik@pdx.edu>
2019-11-30 01:34:53 -0800

    Finishing up function for getting the results after a move. Validated with unit tests.
    Tested a move with no swap, one swap, and many swaps. Made sure that the table was updated, as were the groups as well

--- Final_Project/GameState.py
+++ Final_Project/GameState.py
@@ -37,23 +37,11 @@ class GameState:
         localStoneGroup = self.CopyStoneGroups(self.stoneGroups)
         localStoneGroup[action] = set()
         localStoneGroup[action].add(action)
+        localBoard[actionX][actionY] = self.turn
 
         self.MergeNeighboringStones(action, localStoneGroup, localBoard)
-        # Do it twice, to make sure all neighbors are updated
-        self.MergeNeighboringStones(action, localStoneGroup, localBoard)
 
-
-        # Check to see if surrounding any white groups
-
-        # Update the group and add to current if it is surrounded. Also, update the count of pieces
-        checkedOpposite = [5 * [5 * [0]]]  # keeping track of whether peice is already checked
-        for piece in localStoneGroup[(actionX, actionY)]: # Check all pieces in a group
-            if localBoard[piece[0]+1][piece[1]] is not localBoard[piece[0]][piece[1]] and checkedOpposite[piece[0]+1][piece[1]] == 0:  # If opponents piece is a neighbor
-                for groupMate in localStoneGroup[piece[0]][piece[1]]:  # Make sure that the opponent's piece's group is not trapped
-                    if localBoard[groupMate[0]+1][groupMate[1]] == None:
-                        break
-
-        checkedOpposite = [5 * [5 * [0]]]  # keeping track of whether peice is already checked
+        checkedOpposite = 5 * [5 * [0]]  # keeping track of whether peice is already checked
         for stone in localStoneGroup[action]: # Check all pieces in a group
             stoneX = stone[0]
             stoneY = stone[1]
@@ -68,12 +56,58 @@ class GameState:
                     elif checkedOpposite[x][y] == 1:
                         continue
                     elif localBoard[x][y] is not self.turn:
+                        if self.IsGroupSurrounded(localStoneGroup, localBoard, (x, y)):
+                            self.MergeGroups(localStoneGroup, (x, y), stone)
+                            for oppStone in localStoneGroup[(x, y)]:
+                                oppStoneX = oppStone[0]
+                                oppStoneY = oppStone[1]
+                                localBoard[oppStoneX][oppStoneY] = self.turn
+
+                                for oppX in range(oppStoneX - 1, oppStoneX + 2):
+                                    for oppY in range(oppStoneY - 1, oppStoneY + 2):
+                                        if (abs(oppX - oppStoneX) + abs(oppY - oppStoneY)) is not 1:
+                                            continue
+                                        if oppX < 0 or oppX > 4 or oppY < 0 or oppY > 4:
+                                            continue
+                                        if localBoard[oppX][oppY] is self.turn:
+                                            self.MergeGroups(localStoneGroup, (oppX, oppY), stone)
+
                         for oppStone in localStoneGroup[(x, y)]:
                             oppStoneX = oppStone[0]
                             oppStoneY = oppStone[1]
                             checkedOpposite[oppStoneX][oppStoneY] = 1
 
+        self.board = self.CopyBoard(localBoard)
+        self.stoneGroups = self.CopyStoneGroups(localStoneGroup)
+
+        self.UpdateStoneCount()
+
         return self
+
+    def UpdateStoneCount(self):
+        self.numBlack = 0
+        self.numWhite = 0
+        for row in self.board:
+            for stone in row:
+                if stone == 'B':
+                    self.numBlack += 1
+                elif stone == 'W':
+                    self.numWhite += 1
+
+    def IsGroupSurrounded(self, stoneGroup, board, exampleStoneInGroup):
+        for stone in stoneGroup[exampleStoneInGroup]:
+            stoneX = stone[0]
+            stoneY = stone[1]
+
+            for x in range(stoneX - 1, stoneX + 2):
+                for y in range(stoneY - 1, stoneY + 2):
+                    if (abs(x - stoneX) + abs(y - stoneY)) is not 1:
+                        continue
+                    if x < 0 or x > 4 or y < 0 or y > 4:
+                        continue
+                    if board[x][y] is None:
+                        return False
+        return True
 
     def IsLegal(self, stoneGroup, board, action):
         localBoard = self.CopyBoard(board)
@@ -86,8 +120,7 @@ class GameState:
         localBoard[actionX][actionY] = self.turn
 
         self.MergeNeighboringStones(action, localStoneGroup, localBoard)
-        # Do it twice, to make sure all neighbors are updated
-        self.MergeNeighboringStones(action, localStoneGroup, localBoard)
+
 
 
         edge = False
@@ -122,16 +155,17 @@ class GameState:
     def MergeNeighboringStones(self, action, stoneGroup, board):
         actionX = action[0]
         actionY = action[1]
-        for x in range(actionX - 1, actionX + 2):
-            for y in range(actionY - 1, actionY + 2):
-                if (abs(x - actionX) + abs(y - actionY)) is not 1:
-                    continue
-                if x < 0 or x > 4 or y < 0 or y > 4:
-                    continue
-                if board[x][y] == None:
-                    continue
-                if board[x][y] == board[actionX][actionY]:
-                    self.MergeSetsInDict(stoneGroup, (x, y), (actionX, actionY))
+        for _ in range (2):  # Do it twice, to make sure all neighbors are updated since first pass
+            for x in range(actionX - 1, actionX + 2):
+                for y in range(actionY - 1, actionY + 2):
+                    if (abs(x - actionX) + abs(y - actionY)) is not 1:
+                        continue
+                    if x < 0 or x > 4 or y < 0 or y > 4:
+                        continue
+                    if board[x][y] == None:
+                        continue
+                    if board[x][y] == board[actionX][actionY]:
+                        self.MergeSetsInDict(stoneGroup, (x, y), (actionX, actionY))
 
     def CopyBoard(self, board):
         return deepcopy(board)
